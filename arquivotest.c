9b931ee6937c878cbbdac5cc9d0db3e9f4297ea8
Aulo Cezar <112286959+Aulo007@users.noreply.github.com>
2025-01-17 19:36:55 -0300

    Define pinos no keypad e importar projeto para o picow na extensão

--- arquivotest.c
+++ arquivotest.c
@@ -2,6 +2,17 @@
 #include <stdlib.h>
 #include "LedSequencial.h"
 
+// Definições dos pino do keypad
+
+#define ROW1 13
+#define ROW1 12
+#define ROW1 11
+#define ROW1 10
+#define COL1 9
+#define COL2 8
+#define COL3 7
+#define COL4 6
+
 int main(void)
 {
 
