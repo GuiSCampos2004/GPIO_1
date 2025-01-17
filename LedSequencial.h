822dc1be7df3a61aaae99ea82bd54ac524184814
Aulo Cezar <112286959+Aulo007@users.noreply.github.com>
2025-01-17 19:48:58 -0300

    Algumas correções
    Definir os leds na biblioteca, que tinha definido errado no arquivoteste e criando a configuração na extensão do raspberry para resolver erros

--- LedSequencial.h
+++ LedSequencial.h
@@ -3,7 +3,16 @@
 #include "pico/stdlib.h"
 
 
+// Definições dos pino do keypad
 
+#define ROW1 13
+#define ROW1 12
+#define ROW1 11
+#define ROW1 10
+#define COL1 9
+#define COL2 8
+#define COL3 7
+#define COL4 6
 
 
 #endif
--- arquivotest.c
+++ arquivotest.c
@@ -1,17 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "pico/stdlib.h"
 #include "LedSequencial.h"
 
-// Definições dos pino do keypad
 
-#define ROW1 13
-#define ROW1 12
-#define ROW1 11
-#define ROW1 10
-#define COL1 9
-#define COL2 8
-#define COL3 7
-#define COL4 6
 
 int main(void)
 {
