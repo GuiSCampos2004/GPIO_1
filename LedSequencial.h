373a098bbd3e04ac3449bf56f3da997d08fcdec4
Aulo Cezar <112286959+Aulo007@users.noreply.github.com>
2025-01-17 20:27:20 -0300

    Update LedSequencial.h

--- LedSequencial.h
+++ LedSequencial.h
@@ -14,5 +14,16 @@
 #define COL3 7
 #define COL4 6
 
+// Definições dos pinos dos LEDs RGB
+#define LED1_R 16
+#define LED1_G 17
+#define LED1_B 18
+#define LED2_R 19
+#define LED2_G 20
+#define LED2_B 21
+#define LED3_R 22
+#define LED3_G 26
+#define LED3_B 27
+
 
 #endif
