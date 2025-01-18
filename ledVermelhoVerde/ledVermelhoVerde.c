344174fcc005d42730b42ac173da469039fca91a
kauanmateuss <kamateus77@gmail.com>
2025-01-18 15:00:38 -0300

    I added the functions to turn on the LEDs

--- ledVermelhoVerde/ledVermelhoVerde.c
+++ ledVermelhoVerde/ledVermelhoVerde.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#define led_green_pin 11
+#define led_red_pin 13
+
+// Protótipo da função:
+void led_vemelho_verde(char caractere);
+
 int main()
 {
     stdio_init_all();
@@ -8,5 +14,23 @@ int main()
     while (true) {
         printf("Hello, world!\n");
         sleep_ms(1000);
+    }
+}
+
+
+// Função que liga o led vermelho e verde quando clicar na tecla 6
+
+void led_vermelho_verde(char caractere){
+    if (caractere == 6){
+        // Caso já tenha uma função para inicializar esses pinos, temos que apagar essas inicializações:
+        gpio_init(led_red_pin);
+        gpio_init(led_green_pin);
+        
+        gpio_set_dir(led_green_pin, GPIO_OUT);
+        gpio_set_dir(led_red_pin, GPIO_OUT);
+
+        gpio_put(led_green_pin, 1);
+        gpio_put(led_red_pin, 1);
+        printf("Voce ligou o led vermelho e verde\n");
     }
 }
