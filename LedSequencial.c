e29ae1dd952c31a494e3c52bfba23c204e9d2bfa
Aulo Cezar <112286959+Aulo007@users.noreply.github.com>
2025-01-18 21:35:12 -0300

    Define pinos do teclado e suas teclas

--- LedSequencial.c
+++ LedSequencial.c
@@ -2,8 +2,19 @@
 #include "pico/stdlib.h"
 #include "LedSequencial.h"
 
+// Define os pinos do teclado com as portas GPIO
 
-int main(void)
+uint columns[4] = {13, 12, 11, 10};
+uint roms[4] = {9, 8, 7, 6};
+
+const char keymap[16] = {
+    {'1', '2', '3', 'A'},
+    {'4', '5', '6', 'B'},
+    {'7', '8', '9', 'C'},
+    {'*', '0', '#', 'D'}};
+
+int main()
 {
-    
+    stdio_init_all();
+
 }
