14f6854ea604576c3f0dcd560bba293dadcec269
Igor Almeida <igoralmeida0105@gmail.com>
2025-01-19 10:28:41 -0300

    Cria função 'led_vermelho_azul'

--- LedVermelhoAzul.h
+++ LedVermelhoAzul.h
@@ -1,3 +1,20 @@
 #ifndef LedVermelhoAzul_h
 #define LedVermelhoAzul_h
 #include "pico/stdlib.h"
+
+void led_vermelho_azul (char caractere)
+  { 
+    if (caractere == 7){     
+      gpio_init(led_red_pin);
+      gpio_init(led_blue_pin);
+      
+      gpio_set_dir(led_blue_pin, GPIO_OUT);
+      gpio_set_dir(led_red_pin, GPIO_OUT);
+
+      gpio_put(led_blue_pin, 1);
+      gpio_put(led_red_pin, 1);
+      printf("Voce ligou o led vermelho e azul\n");
+    }
+  }
+
+#endif
