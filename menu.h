32a6a2b7b2591e0af8b1aab375ed1d11c052c580
Aulo Cezar <112286959+Aulo007@users.noreply.github.com>
2025-01-19 02:17:13 -0300

    Atualiza minha biblioteca menu para se adequar ao código

--- menu.h
+++ menu.h
@@ -4,20 +4,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int menu()
+void menu()
 {
-    int escolha;
 
     system("cls");
 
     printf("=== Sistema Iniciado ===\n");
     printf("Pressione uma tecla para escolher uma opcao:\n\n");
     printf("8 - Acender os LEDs sequencialmente\n");
-    printf("\nSua escolha: ");
-
-    scanf("%d", &escolha);
-
-    return escolha;
 }
 
 #endif // MENU_H
