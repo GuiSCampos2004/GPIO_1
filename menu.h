d8f10331e02606d787a8455ff220946160828c1b
Aulo Cezar <112286959+Aulo007@users.noreply.github.com>
2025-01-19 03:06:26 -0300

    Atualiza o menu para funcionar no terminal do raspberry

--- menu.h
+++ menu.h
@@ -2,12 +2,11 @@
 #define MENU_H
 
 #include <stdio.h>
-#include <stdlib.h>
 
 void menu()
 {
-
-    system("cls");
+    // Limpa a tela usando caracteres de escape ANSI
+    printf("\033[2J\033[H"); // Limpa a tela e move o cursor para o início
 
     printf("=== Sistema Iniciado ===\n");
     printf("Pressione uma tecla para escolher uma opcao:\n\n");
