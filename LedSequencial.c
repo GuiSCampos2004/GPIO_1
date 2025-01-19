ceb8b9b045e34c21ad850239cb36e32b935317c1
Aulo Cezar <112286959+Aulo007@users.noreply.github.com>
2025-01-18 22:07:23 -0300

    Adicona função para inicializar o keypad

--- LedSequencial.c
+++ LedSequencial.c
@@ -18,3 +18,28 @@ uint _rows[4];
 char _matrix_values[16];
 uint all_columns_mask = 0x0;
 uint column_mask[4];
+
+//inicializa o keypad
+void pico_keypad_init(uint columns[4], uint rows[4], char matrix_values[16]) {
+
+    for (int i = 0; i < 16; i++) {
+        _matrix_values[i] = matrix_values[i];
+    }
+
+    for (int i = 0; i < 4; i++) {
+
+        _columns[i] = columns[i];
+        _rows[i] = rows[i];
+
+        gpio_init(_columns[i]);
+        gpio_init(_rows[i]);
+
+        gpio_set_dir(_columns[i], GPIO_IN);
+        gpio_set_dir(_rows[i], GPIO_OUT);
+
+        gpio_put(_rows[i], 1);
+
+        all_columns_mask = all_columns_mask + (1 << _columns[i]);
+        column_mask[i] = 1 << _columns[i];
+    }
+}
