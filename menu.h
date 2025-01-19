a8678c60aa8107fe55b96ba20747caefe32b39f9
Aulo Cezar <112286959+Aulo007@users.noreply.github.com>
2025-01-19 02:05:34 -0300

    Define estrutura inicial do menu

--- menu.h
+++ menu.h
@@ -1,9 +1,22 @@
 #ifndef menu_h
 #define menu_h
 
+#include <stdio.h>
+#include <stdlib.h>
 
+int menu()
+{
+    int escolha;
 
+    system("cls");
 
+    printf("Sistema iniciado.Pressione teclas\n");
 
+    printf("Sua escolha: ");
+
+    scanf("%d", &escolha);
+
+    return escolha;
+}
 
 #endif
