eb108319f8e249d5901792c670b8028e57591bef
GuiSCampos2004 <guilherme.fields2004@gmail.com>
2025-01-19 14:42:02 -0300

    Update main2.c

--- main2.c
+++ main2.c
@@ -5,9 +5,15 @@
 uint colunas[4]={0,1,2,3}; //Variáveis necessárias uma vez que funções 
 uint linhas[4]={4,5,6,7}; //gpio precisam de parametros do tipo uint
 
-#define LED_R 13
-#define LED_B 12
-#define LED_G 11
+#define LED1_R 16
+#define LED1_G 17
+#define LED1_B 18
+#define LED2_R 19
+#define LED2_G 20
+#define LED2_B 21
+#define LED3_R 22
+#define LED3_G 26
+#define LED3_B 27
 #define BUZZ 10
 
 //--------------------------------------------------------------------------------------------
