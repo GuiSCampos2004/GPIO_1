54be1cd28a51808438823269f3da21976faae7ee
Aulo Cezar <112286959+Aulo007@users.noreply.github.com>
2025-01-18 22:08:43 -0300

    Update LedSequencial.c

--- LedSequencial.c
+++ LedSequencial.c
@@ -19,6 +19,14 @@ char _matrix_values[16];
 uint all_columns_mask = 0x0;
 uint column_mask[4];
 
+void imprimir_binario(int num) {
+ int i;
+ for (i = 31; i >= 0; i--) {
+  (num & (1 << i)) ? printf("1") : printf("0");
+ }
+}
+
+
 //inicializa o keypad
 void pico_keypad_init(uint columns[4], uint rows[4], char matrix_values[16]) {
 
@@ -41,5 +49,57 @@ void pico_keypad_init(uint columns[4], uint rows[4], char matrix_values[16]) {
 
         all_columns_mask = all_columns_mask + (1 << _columns[i]);
         column_mask[i] = 1 << _columns[i];
+    }
+}
+
+char pico_keypad_get_key(void) {
+    int row;
+    uint32_t cols;
+    bool pressed = false;
+
+    cols = gpio_get_all();
+    cols = cols & all_columns_mask;
+    imprimir_binario(cols);
+    
+    if (cols == 0x0) {
+        return 0;
+    }
+
+    for (int j = 0; j < 4; j++) {
+        gpio_put(_rows[j], 0);
+    }
+
+    for (row = 0; row < 4; row++) {
+
+        gpio_put(_rows[row], 1);
+
+        busy_wait_us(10000);
+
+        cols = gpio_get_all();
+        gpio_put(_rows[row], 0);
+        cols = cols & all_columns_mask;
+        if (cols != 0x0) {
+            break;
+        }   
+    }
+
+    for (int i = 0; i < 4; i++) {
+        gpio_put(_rows[i], 1);
+    }
+
+    if (cols == column_mask[0]) {
+        return (char)_matrix_values[row * 4 + 0];
+    }
+    else if (cols == column_mask[1]) {
+        return (char)_matrix_values[row * 4 + 1];
+    }
+    if (cols == column_mask[2]) {
+        return (char)_matrix_values[row * 4 + 2];
+    }
+    else if (cols == column_mask[3]) {
+        return (char)_matrix_values[row * 4 + 3];
+    }
+    else {
+        return 0;
     }
 }
